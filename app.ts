bace1d12bbeef2968d3fb163aa2255c0e820b4b2
Manuel Jiménez <man.jime@gmail.com>
2020-04-18 17:52:20 +0000

    decouplin app

---
+++ app.ts
@@ -0,0 +1,42 @@
+import tracer from 'dd-trace';
+tracer.init({ analytics: true, logInjection: true, runtimeMetrics: true });
+
+import * as express from 'express';
+import axios from 'axios';
+import * as os from 'os';
+
+const app = express();
+
+function fibo(n: number): number {
+  if (n < 2) return 1;
+  else return fibo(n - 2) + fibo(n - 1);
+}
+
+app.get('/fibo/:num', (req: express.Request, res: express.Response) => {
+  res.send('Result: ' + fibo(+req.params.num));
+});
+
+app.get('/cpus', (req: express.Request, res: express.Response) => {
+  res.send(os.cpus());
+});
+
+app.get('/sw/:id', async (req: express.Request, res: express.Response) => {
+  const response = await axios.get('https://swapi.dev/api/planets/' + req.params.id);
+  res.send(response.data);
+});
+
+app.get('/work', (req: express.Request, res: express.Response) => {
+  var i = 0;
+  while (i < 1e5) i++;
+  res.send('i: ' + i);
+});
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  console.log(`Worker ${process.pid} running`);
+  res.send('Hello World!');
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
 import * as cluster from 'cluster';
 import * as os from 'os';
 
-import { Request, Response } from 'express';
-
 if (cluster.isMaster) {
   const numCPUs = os.cpus().length;
 
@@ -12,40 +10,8 @@ if (cluster.isMaster) {
 
   cluster.on('exit', (worker, code, signal) => {
     console.log('Worker %d died :(', worker.id, code, signal);
-    // cluster.fork();
+    cluster.fork();
   });
 } else {
-  require('dd-trace').init({ analytics: true, logInjection: true, runtimeMetrics: true });
-
-  const express = require('express');
-  const app = express();
-
-  function fibo(n: number): number {
-    if (n < 2) return 1;
-    else return fibo(n - 2) + fibo(n - 1);
-  }
-
-  app.get('/fibo/:num', (req: Request, res: Response) => {
-    res.send('Result: ' + fibo(+req.params.num));
-  });
-
-  app.get('/cpus', (req: Request, res: Response) => {
-    res.send(os.cpus());
-  });
-
-  app.get('/work', (req: Request, res: Response) => {
-    var i = 0;
-    while (i < 1e5) i++;
-    res.send('i: ' + i);
-  });
-
-  app.get('/', (req: Request, res: Response) => {
-    console.log(`Worker ${process.pid} running`);
-    res.send('Hello World!');
-  });
-
-  const port = process.env.PORT || 8080;
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
-  });
+  require('./app');
 }
