7a954a5e2639d0b4c64fa23454588be3cb672670
Manuel Jiménez <man.jime@gmail.com>
2020-04-18 14:29:29 +0000

    delete ignored files

--- tracer.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dd_trace_1 = require("dd-trace");
-dd_trace_1.default.init();
-exports.default = dd_trace_1.default;
