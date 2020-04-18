3ea59ec70bc08a2d87a885e5bc93b75e4ffee182
Manuel Jiménez <man.jime@gmail.com>
2020-04-18 15:24:35 +0000

    commit

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ibmcloud cf push DataDogApp --no-start -b https://github.com/cloudfoundry/binary-buildpack.git
-ibmcloud cf v3-push DataDogApp -b https://cloudfoundry.datadoghq.com/datadog-cloudfoundry-buildpack/datadog-cloudfoundry-buildpack-latest.zip -b https://github.com/cloudfoundry/nodejs-buildpack.git
+ibmcloud cf push DataDogApp --no-start -b binary_buildpack --vars-file vars.yaml
+ibmcloud cf v3-push DataDogApp -b https://cloudfoundry.datadoghq.com/datadog-cloudfoundry-buildpack/datadog-cloudfoundry-buildpack-latest.zip -b sdk-for-nodejs
